16fc5ba5a3b36004b622dd80c2b8f2912a089559
lhuangufl <lhuangufl@gmail.com>
2022-04-02 15:43:56 -0400

    Add Join, Sum, Project operators

--- P3/RelOp.h
+++ P3/RelOp.h
@@ -7,69 +7,93 @@
 #include "Function.h"
 
 class RelationalOp {
-	public:
-	// blocks the caller until the particular relational operator 
+private:
+	pthread_t thread;
+
+public:
+	// blocks the caller until the particular relational operator
 	// has run to completion
-	virtual void WaitUntilDone () = 0;
+	virtual void WaitUntilDone() = 0;
 
 	// tell us how much internal memory the operation can use
-	virtual void Use_n_Pages (int n) = 0;
+	virtual void Use_n_Pages(int n) = 0;
 };
 
-class SelectFile : public RelationalOp { 
+class SelectFile : public RelationalOp {
 
-	private:
+private:
 	// pthread_t thread;
 	// Record *buffer;
 
-	public:
 
-	void Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal);
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
+public:
+
+	void Run(DBFile& inFile, Pipe& outPipe, CNF& selOp, Record& literal);
+	void WaitUntilDone();
+	void Use_n_Pages(int n);
 
 };
 
 class SelectPipe : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+private:
+
+public:
+	void Run(Pipe& inPipe, Pipe& outPipe, CNF& selOp, Record& literal) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
-class Project : public RelationalOp { 
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class Project : public RelationalOp {
+private:
+
+public:
+	void Run(Pipe& inPipe, Pipe& outPipe, int* keepMe, int numAttsInput, int numAttsOutput) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
-class Join : public RelationalOp { 
-	public:
-	void Run (Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+class Join : public RelationalOp {
+private:
+	int runlen;
+public:
+	void Run(Pipe& inPipeL, Pipe& inPipeR, Pipe& outPipe, CNF& selOp, Record& literal) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
 class DuplicateRemoval : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+private:
+	int runlen;
+public:
+	void Run(Pipe& inPipe, Pipe& outPipe, Schema& mySchema) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
 class Sum : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+private:
+
+
+public:
+	void Run(Pipe& inPipe, Pipe& outPipe, Function& computeMe) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
 class GroupBy : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+private:
+	Pipe* inputPipe;
+	Pipe* outputPipe;
+	OrderMaker* orderMaker;
+	Function* func;
+public:
+	void Run(Pipe& inPipe, Pipe& outPipe, OrderMaker& groupAtts, Function& computeMe) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
 class WriteOut : public RelationalOp {
-	public:
-	void Run (Pipe &inPipe, FILE *outFile, Schema &mySchema) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+private:
+	Pipe* inputPipe;
+	FILE* outputFile;
+	Sc	hema* schema;
+public:
+	void Run(Pipe& inPipe, FILE* outFile, Schema& mySchema) { }
+	void WaitUntilDone() { }
+	void Use_n_Pages(int n) { }
 };
 #endif
