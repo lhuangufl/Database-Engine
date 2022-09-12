49488a59c98af9484b86e0204a8dfeba35d2a70b
Lin Huang <lhuangufl@gmail.com>
2022-09-12 09:23:27 -0400

    Fix bugs

--- P1/DBFile.h
+++ P1/DBFile.h
@@ -13,30 +13,31 @@ typedef enum { heap, sorted, tree } fType;
 // stub DBFile header..replace it with your own DBFile.h
 
 class DBFile {
-	  char file_path[20];
-	  Record *currentRecord;
-	  Page *readPage;
-	  Page *writePage;
-	  File *file;
-	  off_t pageIndex;
-	  off_t writeIndex;
-	  char *name;
-	  bool writeIsDirty;
-	  bool endOfFile;
+private:
+	char file_path [20];
+	Record* currentRecord;
+	Page* readPage;
+	Page* writePage;
+	File* file;
+	off_t pageIndex;
+	off_t writeIndex;
+	char* name;
+	bool writeIsDirty;
+	bool endOfFile;
 
-        public:
-	  DBFile();
-	  ~DBFile();
+public:
+	DBFile();
+	~DBFile();
 
-	  int Create(char *fpath, fType file_type, void *startup);
-	  int Open(char *fpath);
-	  int Close();
+	int Create(char* fpath, fType file_type, void* startup);
+	int Open(char* fpath);
+	int Close();
 
-	  void Load(Schema &myschema, char *loadpath);
+	void Load(Schema& myschema, char* loadpath);
 
-	  void MoveFirst();
-	  void Add(Record &addme);
-	  int GetNext(Record &fetchme);
-	  int GetNext(Record &fetchme, CNF &cnf, Record &literal);
+	void MoveFirst();
+	void Add(Record& addme);
+	int GetNext(Record& fetchme);
+	int GetNext(Record& fetchme, CNF& cnf, Record& literal);
 };
 #endif
